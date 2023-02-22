e018b991679a3dc2f423cbf5fb273b187af5497a
PATRICK JHORNES FALEIRO CASAGRANDE <patrickcasagrande0@gmail.com>
2023-02-21 20:55:34 -0400

    Main

---
+++ app.py
@@ -0,0 +1,7 @@
+#   print(' hello word')
+nome = str(input(('Qual seu nome ')))
+idade = int(input(('Qual sua idade ')))
+mae = str(input((' Qual nome da sua Mãe ')))
+prof = str(input(('Qual sua profissão ')))
+print('Seu nome é', nome, 'e sua idade atual é', idade, 'anos, nome da sua Mãe é', mae, ' e sua Profissão atual informada', prof)
+
